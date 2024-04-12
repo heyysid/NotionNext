04fd3f0288b34f25ccf5c2b3ca8c468793bf8d28
BD7PCI <112547623+heyysid@users.noreply.github.com>
2024-04-13 00:34:56 +0800

    Update Footer.js

--- themes/example/components/Footer.js
+++ themes/example/components/Footer.js
@@ -1,4 +1,3 @@
-import DarkModeButton from '@/components/DarkModeButton'
 import { siteConfig } from '@/lib/config'
 
 export const Footer = (props) => {
@@ -7,12 +6,13 @@ export const Footer = (props) => {
   const since = siteConfig('SINCE')
   const copyrightDate = parseInt(since) < currentYear ? since + '-' + currentYear : currentYear
 
-  return <footer className="z-10 relative w-full bg-white px-6 border-t dark:border-hexo-black-gray dark:bg-hexo-black-gray ">
-        <DarkModeButton className='text-center pt-4'/>
-
-        <div className="container mx-auto max-w-4xl py-6 md:flex flex-wrap md:flex-no-wrap md:justify-between items-center text-sm">
-            <div className='text-center'> &copy;{`${copyrightDate}`} {siteConfig('户外摄影、骑行、背包徒步、业余无线电，马督工粉丝')}. All rights reserved.
-            </div>
+  return (
+    <footer className="z-10 relative w-full bg-white px-6 border-t dark:border-hexo-black-gray dark:bg-hexo-black-gray ">
+      <div className="container mx-auto max-w-4xl py-6 md:flex flex-wrap md:flex-no-wrap md:justify-between items-center text-sm">
+        <div className='text-center'>
+          &copy;{`${copyrightDate}`} {siteConfig('AUTHOR')}. All rights reserved.
         </div>
+      </div>
     </footer>
+  )
 }
