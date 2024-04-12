8ca22808cb6efc5565ab571597de70348a7a59bf
BD7PCI <112547623+heyysid@users.noreply.github.com>
2024-04-13 02:10:24 +0800

    Update Title.js

--- themes/example/components/Title.js
+++ themes/example/components/Title.js
@@ -8,13 +8,15 @@ import { siteConfig } from '@/lib/config'
  */
 export const Title = (props) => {
   const { post } = props
-  const title = post?.title || siteConfig('户外摄影、骑行、背包徒步、业余无线电，马督工粉丝')
-  const description = post?.description || siteConfig('Li ZhongPing.')
+  const title = post?.title || siteConfig("Here is Li Zhongping's blog.")
+  const description = post ? "Li ZhongPing." : "户外摄影、骑行、背包徒步、业余无线电，马督工粉丝"
 
-  return <div className="text-center px-6 py-12 mb-6 bg-gray-100 dark:bg-hexo-black-gray dark:border-hexo-black-gray border-b">
-        <h1 className="text-xl md:text-4xl pb-4"><NotionIcon icon={post?.pageIcon} />{title}</h1>
-        <p className="leading-loose text-gray-dark">
-            {/* */}
-        </p>
+  return (
+    <div className="text-center px-6 py-12 mb-6 bg-gray-100 dark:bg-hexo-black-gray dark:border-hexo-black-gray border-b">
+      <h1 className="text-xl md:text-4xl pb-4"><NotionIcon icon={post?.pageIcon} />{title}</h1>
+      <p className="leading-loose text-gray-dark">
+        {description}
+      </p>
     </div>
+  )
 }
