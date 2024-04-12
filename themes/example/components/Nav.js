e2b0888086b6e3f6219db2036b5a6193d5999047
BD7PCI <112547623+heyysid@users.noreply.github.com>
2024-04-13 01:38:33 +0800

    Update Nav.js

--- themes/example/components/Nav.js
+++ themes/example/components/Nav.js
@@ -32,16 +32,12 @@ export const Nav = (props) => {
     return null
   }
 
-  return (
-        <nav className="w-full bg-white md:pt-0 px-6 relative z-20 border-t border-b border-gray-light dark:border-hexo-black-gray dark:bg-black">
-            <div className="container mx-auto max-w-4xl md:flex justify-between items-center text-sm md:text-md md:justify-start">
-                <ul className="w-full text-center md:text-left flex flex-wrap justify-center items-stretch md:justify-start md:items-start">
-                    {links.map((link, index) => <MenuItemDrop key={index} link={link} />)}
-                </ul>
-                {/* <div className="w-full md:w-1/3 text-center md:text-right"> */}
-                    {/* <!-- extra links --> */}
-                {/* </div> */}
-            </div>
-        </nav>
-  )
-}
+return (
+  <nav className="w-full bg-white md:pt-0 px-6 relative z-20 border-t border-b border-gray-light dark:border-hexo-black-gray dark:bg-black">
+    <div className="container mx-auto max-w-4xl md:flex justify-between items-center text-sm md:text-md md:justify-end">
+      <ul className="w-full text-center md:text-left flex flex-wrap justify-center items-stretch md:justify-end md:items-start">
+        {links.map((link, index) => <MenuItemDrop key={index} link={link} />)}
+      </ul>
+    </div>
+  </nav>
+)
