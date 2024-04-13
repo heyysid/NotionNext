ff1e859efee9e515b8be91e460f86d0a7a8c166a
BD7PCI <112547623+heyysid@users.noreply.github.com>
2024-04-13 17:28:00 +0800

    Update Nav.js

--- themes/example/components/Nav.js
+++ themes/example/components/Nav.js
@@ -28,15 +28,14 @@ export const Nav = (props) => {
   }
   
   return (
-    <nav className="w-full bg-white dark:border-hexo-black-gray dark:bg-black">
-      <div className="md:flex text-sm md:text-md">
+    <nav className="bg-white dark:border-hexo-black-gray dark:bg-black">
         {/* 电脑端布局，绝对定位在顶部最右侧 */}
-        <ul className="hidden md:flex md:justify-end md:items-start absolute top-0 right-0">
+        <ul className="hidden md:flex md:justify-end md:items-start md:absolute md:top-0 md:right-0">
           {links.map((link, index) => <MenuItemDrop key={index} link={link} />)}
         </ul>
 
         {/* 移动端布局 */}
-        <ul className="md:hidden flex top-0 left-0">
+        <ul className="flex top-0 left-0">
           {links.map((link, index) => <MenuItemDrop key={index} link={link} />)}
         </ul>
       </div>
