7e3a7f98a0aa8c4ac09caa3b0d8bf3d9adb446c4
BD7PCI <112547623+heyysid@users.noreply.github.com>
2024-04-14 00:25:12 +0800

    Update SideBar.js

--- themes/example/components/SideBar.js
+++ themes/example/components/SideBar.js
@@ -4,69 +4,97 @@ import { useGlobal } from '@/lib/global'
 import Link from 'next/link'
 import dynamic from 'next/dynamic'
 import Announcement from './Announcement'
-import { InfoCard } from './InfoCard' // 导入 InfoCard 组件
-const ExampleRecentComments = dynamic(() => import('./ExampleRecentComments'))
+import { siteConfig } from '@/lib/config'
 
+const ExampleRecentComments = dynamic(() => import('./ExampleRecentComments'))
 export const SideBar = (props) => {
   const { locale } = useGlobal()
   const { latestPosts, categoryOptions, notice } = props
   return (
-    <div className="w-full md:w-64 sticky top-8">
-      <aside className="rounded shadow overflow-hidden mb-6">
-        <h3 className="text-sm bg-gray-100 text-gray-700 dark:bg-hexo-black-gray dark:text-gray-200 py-3 px-4 dark:border-hexo-black-gray border-b">{locale.COMMON.CATEGORY}</h3>
-        <div className="p-4">
-          <ul className="list-reset leading-normal">
-            {categoryOptions?.map(category => {
-              return (
-                <Link
-                  key={category.name}
-                  href={`/category/${category.name}`}
-                  passHref
-                  legacyBehavior
-                >
-                  <li><a href={`/category/${category.name}`} className="text-gray-darkest text-sm">{category.name}({category.count})</a></li>
-                </Link>
-              )
-            })}
-          </ul>
-        </div>
-      </aside>
-
-      <aside className="rounded shadow overflow-hidden mb-6">
-        <h3 className="text-sm bg-gray-100 text-gray-700 dark:bg-hexo-black-gray dark:text-gray-200 py-3 px-4 dark:border-hexo-black-gray border-b">{locale.COMMON.LATEST_POSTS}</h3>
-        <div className="p-4">
-          <ul className="list-reset leading-normal">
-            {latestPosts?.map(p => {
-              return (
-                <div key={p.id}>
-                  <Link key={p.id} href={`/${p.slug}`} passHref legacyBehavior>
-                    <li><a href={`/${p.slug}`} className="text-gray-darkest text-sm">{p.title}</a></li>
-                  </Link>
-                  <hr className="mt-2 border-gray-300 dark:border-gray-600"/>
+      <div className="w-full md:w-64 sticky top-8">
+    
+            <aside className="rounded shadow overflow-hidden mb-6">
+                <h3 className="text-sm bg-gray-100 text-gray-700 dark:bg-hexo-black-gray dark:text-gray-200 py-3 px-4 dark:border-hexo-black-gray border-b">{locale.COMMON.CATEGORY}</h3>
+                <div className="p-4">
+                    <ul className="list-reset leading-normal">
+                        {categoryOptions?.map(category => {
+                          return (
+                              <Link
+                                  key={category.name}
+                                  href={`/category/${category.name}`}
+                                  passHref
+                                  legacyBehavior>
+                                    <li>  <a href={`/category/${category.name}`} className="text-gray-darkest text-sm">{category.name}({category.count})</a></li>
+                                </Link>
+                          )
+                        })}
+                    </ul>
                 </div>
-              )
-            })}
-          </ul>
+            </aside>
+                      
+            <aside className="rounded shadow overflow-hidden mb-6">
+                <h3 className="text-sm bg-gray-100 text-gray-700 dark:bg-hexo-black-gray dark:text-gray-200 py-3 px-4 dark:border-hexo-black-gray border-b">{locale.COMMON.LATEST_POSTS}</h3>
+                <div className="p-4">
+                    <ul className="list-reset leading-normal">
+                        {latestPosts?.map(p => {
+                          return (
+                              <Link key={p.id} href={`/${p.slug}`} passHref legacyBehavior>
+                                    <li>  <a href={`/${p.slug}`} className="text-gray-darkest text-sm">{p.title}</a></li>
+                                </Link>
+                          )
+                            return (
+                                <div key={p.id}> {/* 添加一个 div 元素包裹 Link 和 hr 元素 */}
+                                    <Link key={p.id} href={`/${p.slug}`} passHref legacyBehavior>
+                                        <li>
+                                            <a href={`/${p.slug}`} className="text-gray-darkest text-sm">{p.title}</a>
+                                        </li>
+                                    </Link>
+                                    <hr className="mt-2 border-gray-300 dark:border-gray-600"/> {/* 下划线 */}
+                                </div>
+                            )
+                        })}
+                    </ul>
+                </div>
+            </aside>
+                      
+<aside className='w-full justify-center flex-wrap flex'>
+    <div className='space-x-3 text-xl text-gray-600 dark:text-gray-300 '>
+      {siteConfig('CONTACT_GITHUB') && <a target='_blank' rel='noreferrer' title={'github'} href={siteConfig('CONTACT_GITHUB')} >
+        <i className='transform hover:scale-125 duration-150 fab fa-github dark:hover:text-indigo-400 hover:text-indigo-600'/>
+      </a>}
+      {siteConfig('CONTACT_TWITTER') && <a target='_blank' rel='noreferrer' title={'twitter'} href={siteConfig('CONTACT_TWITTER')} >
+        <i className='transform hover:scale-125 duration-150 fab fa-twitter dark:hover:text-indigo-400 hover:text-indigo-600'/>
+      </a>}
+      {siteConfig('CONTACT_TELEGRAM') && <a target='_blank' rel='noreferrer' href={siteConfig('CONTACT_TELEGRAM')} title={'telegram'} >
+        <i className='transform hover:scale-125 duration-150 fab fa-telegram dark:hover:text-indigo-400 hover:text-indigo-600'/>
+      </a>}
+      {siteConfig('CONTACT_LINKEDIN') && <a target='_blank' rel='noreferrer' href={siteConfig('CONTACT_LINKEDIN')} title={'linkIn'} >
+        <i className='transform hover:scale-125 duration-150 fab fa-linkedin dark:hover:text-indigo-400 hover:text-indigo-600'/>
+      </a>}
+      {siteConfig('CONTACT_WEIBO') && <a target='_blank' rel='noreferrer' title={'weibo'} href={siteConfig('CONTACT_WEIBO')} >
+        <i className='transform hover:scale-125 duration-150 fab fa-weibo dark:hover:text-indigo-400 hover:text-indigo-600'/>
+      </a>}
+      {siteConfig('CONTACT_INSTAGRAM') && <a target='_blank' rel='noreferrer' title={'instagram'} href={siteConfig('CONTACT_INSTAGRAM')} >
+        <i className='transform hover:scale-125 duration-150 fab fa-instagram dark:hover:text-indigo-400 hover:text-indigo-600'/>
+      </a>}
+      {siteConfig('CONTACT_EMAIL') && <a target='_blank' rel='noreferrer' title={'email'} href={`mailto:${siteConfig('CONTACT_EMAIL')}`} >
+        <i className='transform hover:scale-125 duration-150 fas fa-envelope dark:hover:text-indigo-400 hover:text-indigo-600'/>
+      </a>}
+      {JSON.parse(siteConfig('ENABLE_RSS')) && <a target='_blank' rel='noreferrer' title={'RSS'} href={'/feed'} >
+        <i className='transform hover:scale-125 duration-150 fas fa-rss dark:hover:text-indigo-400 hover:text-indigo-600'/>
+      </a>}
+      {siteConfig('CONTACT_BILIBILI') && <a target='_blank' rel='noreferrer' title={'bilibili'} href={siteConfig('CONTACT_BILIBILI')} >
+        <i className='transform hover:scale-125 duration-150 fab fa-bilibili dark:hover:text-indigo-400 hover:text-indigo-600'/>
+      </a>}
+      {siteConfig('CONTACT_YOUTUBE') && <a target='_blank' rel='noreferrer' title={'youtube'} href={siteConfig('CONTACT_YOUTUBE')} >
+        <i className='transform hover:scale-125 duration-150 fab fa-youtube dark:hover:text-indigo-400 hover:text-indigo-600'/>
+      </a>}
         </div>
-      </aside>
-
-      {/* 将第三个 aside 渲染为 InfoCard 包含社交信息的卡片 */}
-      <aside className="rounded shadow overflow-hidden mb-6">
-        <InfoCard className="mb-6" siteInfo={siteConfig} />
-      </aside>
-
-      {siteConfig('COMMENT_WALINE_SERVER_URL') && siteConfig('COMMENT_WALINE_RECENT') && 
-        <aside className="rounded shadow overflow-hidden mb-6">
-          <h3 className="text-sm bg-gray-100 text-gray-700 dark:bg-hexo-black-gray dark:text-gray-200 py-3 px-4 dark:border-hexo-black-gray border-b">{locale.COMMON.RECENT_COMMENTS}</h3>
-          <div className="p-4">
-            <ExampleRecentComments/>
-          </div>
-        </aside>
-      }
-
-      <aside className="rounded overflow-hidden mb-6">
-        <Live2D />
-      </aside>
-    </div>
+    </aside>
+                             
+            <aside className="rounded  overflow-hidden mb-6">
+                <Live2D />
+            </aside>
+        </div>
   )
 }
