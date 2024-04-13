79349c3bae2ec1e33c22a0403f913c30fb588336
BD7PCI <112547623+heyysid@users.noreply.github.com>
2024-04-13 21:50:33 +0800

    Update Header.js

--- themes/example/components/Header.js
+++ themes/example/components/Header.js
@@ -6,15 +6,15 @@ import Link from 'next/link'
  */
 export const Header = (props) => {
   return (
-    <header className="w-full px-6 bg-white  dark:bg-black relative top-0 z-10">
+    <header className="w-full px-6 bg-white  dark:bg-black relative top-0 z-10 h-0  hidden">
       <div className="container mx-auto max-w-4xl md:flex justify-between items-center">
         <Link 
             href='/' 
             className="py-6 w-full text-center md:text-left md:w-auto text-gray-dark no-underline flex justify-center items-center">
-            'cheshi22'
+          {/* 空的文本，不显示任何字符 */}
         </Link>
         <div className="w-full md:w-auto text-center md:text-right">
-          'cheshi'
+          {/* 右侧文字 */}
         </div>
       </div>
     </header>
