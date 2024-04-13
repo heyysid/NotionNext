6794ebc32c7a7ac283b3c897e0e7f06fb3ef3a02
BD7PCI <112547623+heyysid@users.noreply.github.com>
2024-04-13 23:43:59 +0800

    Update Footer.js

--- themes/example/components/Footer.js
+++ themes/example/components/Footer.js
@@ -1,4 +1,5 @@
 import { siteConfig } from '@/lib/config'
+import DarkModeButton from '@/components/DarkModeButton'
 
 export const Footer = (props) => {
   const d = new Date()
@@ -8,6 +9,7 @@ export const Footer = (props) => {
 
   return (
     <footer className="z-10 relative w-full bg-white px-6 border-t dark:border-hexo-black-gray dark:bg-hexo-black-gray ">
+    <DarkModeButton className='text-center pt-4'/>
       <div className="container mx-auto max-w-4xl py-6 md:flex flex-wrap md:flex-no-wrap md:justify-between items-center text-sm">
         <div className='text-center'>
           &copy;{`${copyrightDate}`} {siteConfig('AUTHOR')}. All rights reserved.
