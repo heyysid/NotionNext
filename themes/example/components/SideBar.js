b4b86194e6443546ebb7584f4adfa7b884916f4a
BD7PCI <112547623+heyysid@users.noreply.github.com>
2024-04-14 00:37:52 +0800

    Update SideBar.js

--- themes/example/components/SideBar.js
+++ themes/example/components/SideBar.js
@@ -47,6 +47,7 @@ export const SideBar = (props) => {
       </aside>
 
       <aside className='w-full justify-center flex-wrap flex'>
+        <h3 className="text-sm bg-gray-100 text-gray-700 dark:bg-hexo-black-gray dark:text-gray-200 py-3 px-4 dark:border-hexo-black-gray border-b">'社交媒体覆盖'</h3>
         <div className='space-x-3 text-xl text-gray-600 dark:text-gray-300 '>
           {siteConfig('CONTACT_GITHUB') && <a key="github" target='_blank' rel='noreferrer' title={'github'} href={siteConfig('CONTACT_GITHUB')}>
             <i className='transform hover:scale-125 duration-150 fab fa-github dark:hover:text-indigo-400 hover:text-indigo-600'/>
