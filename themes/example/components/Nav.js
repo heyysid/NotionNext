fd9ce12ddab12c2a3e899a0e8a2cc8808edc2788
BD7PCI <112547623+heyysid@users.noreply.github.com>
2024-04-13 17:43:50 +0800

    Update Nav.js

--- themes/example/components/Nav.js
+++ themes/example/components/Nav.js
@@ -47,12 +47,12 @@ return (
   <nav className="w-full bg-white dark:border-hexo-black-gray dark:bg-black">
     <div className="text-sm md:flex md:text-md">
       {/* 电脑端布局，绝对定位在顶部最右侧 */}
-      <ul className="hidden md:flex md:absolute md:top-0 md:right-0">
+      <ul className="hidden md:flex md:justify-end md:items-start absolute top-0 right-0">
         {links.map((link, index) => <MenuItemDrop key={index} link={link} />)}
       </ul>
 
       {/* 移动端布局 */}
-      <ul className="flex top-0 left-0 md:hidden">
+      <ul className="flex absolute top-0 left-0 md:hidden">
         {links.map((link, index) => <MenuItemDrop key={index} link={link} />)}
       </ul>
     </div>
