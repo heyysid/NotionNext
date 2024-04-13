f77b3c77c935c6c972f03250bd060803d8530af6
BD7PCI <112547623+heyysid@users.noreply.github.com>
2024-04-14 01:08:01 +0800

    Update SideBar.js

--- themes/example/components/SideBar.js
+++ themes/example/components/SideBar.js
@@ -46,9 +46,9 @@ export const SideBar = (props) => {
         </div>
       </aside>
 
-      <aside className='rounded shadow flex justify-center overflow-hidden mb-6'>
+      <aside className='rounded shadow flex justify-center overflow-hidden mb-6 w-full flex-wrap'>
         <h3 className="text-sm bg-gray-100 text-gray-700 dark:bg-hexo-black-gray dark:text-gray-200 py-3 px-4 dark:border-hexo-black-gray border-b mb-6">社交媒体覆盖</h3>
-        <div className='space-x-3 text-xl text-gray-600 dark:text-gray-300 p-4'>
+        <div className='space-x-3 text-xl text-gray-600 dark:text-gray-300'>
           {siteConfig('CONTACT_GITHUB') && <a key="github" target='_blank' rel='noreferrer' title={'github'} href={siteConfig('CONTACT_GITHUB')}>
             <i className='transform hover:scale-125 duration-150 fab fa-github dark:hover:text-indigo-400 hover:text-indigo-600'/>
           </a>}
