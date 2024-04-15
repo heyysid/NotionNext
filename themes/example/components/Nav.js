0a9d33ec8a18a1351b2e41ac3ff547e8a90ba4a2
BD7PCI <112547623+heyysid@users.noreply.github.com>
2024-04-15 12:43:19 +0800

    Update Nav.js

--- themes/example/components/Nav.js
+++ themes/example/components/Nav.js
@@ -34,8 +34,8 @@ export const Nav = (props) => {
   
   return (
     <nav className="w-full bg-white md:pt-0 px-6 relative z-20 border-t border-b border-gray-light dark:border-hexo-black-gray dark:bg-black">
-      <div className="container mx-auto max-w-4xl md:flex text-sm md:text-md">
-        <ul className="flex absolute top-0 left text-sm md:flex md:text-md md:absolute md:top-0 md:right-0">
+      <div className="container mx-auto max-w-4xl md:flex text-xs md:text-md">
+        <ul className="sm:flex sm:absolute sm:top sm:left-1 sm:text-xs flex text-md absolute top-0 right-0">
           {links.map((link, index) => (
             <MenuItemDrop key={index} link={link} />
           ))}
