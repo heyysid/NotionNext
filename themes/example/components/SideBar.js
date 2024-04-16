f5e80a12d350034c65871855a74eb3cbd576315b
BD7PCI <112547623+heyysid@users.noreply.github.com>
2024-04-16 14:32:33 +0800

    Update SideBar.js

--- themes/example/components/SideBar.js
+++ themes/example/components/SideBar.js
@@ -14,7 +14,7 @@ export const SideBar = (props) => {
   return (
     <div className="w-full md:w-64 sticky top-8">
       <aside className="rounded shadow overflow-hidden mb-6">
-        <h3 className="text-sm bg-gray-100 text-gray-700 dark:bg-hexo-black-gray dark:text-gray-200 py-3 px-4 dark:border-hexo-black-gray border-b">{locale.COMMON.CATEGORY}</h3>
+        <h3 className="text-sm bg-gray-100 text-gray-700 dark:bg-hexo-black-gray dark:text-gray-200 py-3 px-4 dark:border-hexo-black-gray border-b">最近更新</h3>
         <div className="p-4">
           <ul className="list-reset leading-normal">
             {categoryOptions?.map(category => (
