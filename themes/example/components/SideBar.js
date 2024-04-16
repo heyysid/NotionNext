8648b675d063893ec5806ec7a644fffba748f1e3
BD7PCI <112547623+heyysid@users.noreply.github.com>
2024-04-16 20:18:42 +0800

    Update SideBar.js

--- themes/example/components/SideBar.js
+++ themes/example/components/SideBar.js
@@ -17,17 +17,33 @@ export const SideBar = (props) => {
         <h3 className="text-sm bg-gray-100 text-gray-700 dark:bg-hexo-black-gray dark:text-gray-200 py-3 px-4 dark:border-hexo-black-gray border-b">{locale.COMMON.TAGS}</h3>
         <div className="p-4">
           <ul className="list-reset leading-normal">
-            {tagsptions?.map(tags => (
-              <li key={tags.name}>
-                <Link href={`/tags/${tags.name}`} passHref>
-                  <a className="text-gray-darkest text-sm">#{tags.name}({tags.count})</a>
+            {tagptions?.map(tag => (
+              <li key={tag.name}>
+                <Link href={`/tag/${tag.name}`} passHref>
+                  <a className="text-gray-darkest text-sm">#{tag.name}({tag.count})</a>
                 </Link>
               </li>
             ))}
           </ul>
         </div>
       </aside>
-
+            
+    <div className="w-full md:w-64 sticky top-8">
+      <aside className="rounded shadow overflow-hidden mb-6">
+        <h3 className="text-sm bg-gray-100 text-gray-700 dark:bg-hexo-black-gray dark:text-gray-200 py-3 px-4 dark:border-hexo-black-gray border-b">{locale.COMMON.CATEGORY}</h3>
+        <div className="p-4">
+          <ul className="list-reset leading-normal">
+            {categoryptions?.map(category => (
+              <li key={category.name}>
+                <Link href={`/category/${category.name}`} passHref>
+                  <a className="text-gray-darkest text-sm">#{category.name}({category.count})</a>
+                </Link>
+              </li>
+            ))}
+          </ul>
+        </div>
+      </aside>
+            
       <aside className="rounded shadow overflow-hidden mb-6">
         <h3 className="text-sm bg-gray-100 text-gray-700 dark:bg-hexo-black-gray dark:text-gray-200 py-3 px-4 dark:border-hexo-black-gray border-b">最近更新</h3>
         <div className="p-4">
