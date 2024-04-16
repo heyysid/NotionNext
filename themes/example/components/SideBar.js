7ec302ad664b92866e7079154e1f89d7ca9e6eae
BD7PCI <112547623+heyysid@users.noreply.github.com>
2024-04-16 20:11:10 +0800

    Update SideBar.js

--- themes/example/components/SideBar.js
+++ themes/example/components/SideBar.js
@@ -9,18 +9,18 @@ const ExampleRecentComments = dynamic(() => import('./ExampleRecentComments'))
 
 export const SideBar = (props) => {
   const { locale } = useGlobal()
-  const { latestPosts, categoryOptions, notice } = props
+  const { latestPosts, categoryOptions, notice, tagsptions } = props
   
   return (
     <div className="w-full md:w-64 sticky top-8">
       <aside className="rounded shadow overflow-hidden mb-6">
-        <h3 className="text-sm bg-gray-100 text-gray-700 dark:bg-hexo-black-gray dark:text-gray-200 py-3 px-4 dark:border-hexo-black-gray border-b">{locale.COMMON.CATEGORY}</h3>
+        <h3 className="text-sm bg-gray-100 text-gray-700 dark:bg-hexo-black-gray dark:text-gray-200 py-3 px-4 dark:border-hexo-black-gray border-b">{locale.COMMON.TAGS}</h3>
         <div className="p-4">
           <ul className="list-reset leading-normal">
-            {categoryptions?.map(category => (
-              <li key={category.name}>
-                <Link href={`/category/${category.name}`} passHref>
-                  <a className="text-gray-darkest text-sm">#{category.name}({category.count})</a>
+            {tagsptions?.map(tags => (
+              <li key={tags.name}>
+                <Link href={`/tags/${tags.name}`} passHref>
+                  <a className="text-gray-darkest text-sm">#{tags.name}({tags.count})</a>
                 </Link>
               </li>
             ))}
