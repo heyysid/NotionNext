ac0d5a7b49f9eeb3c7b73e1c1b47bbac56fb33e7
BD7PCI <112547623+heyysid@users.noreply.github.com>
2024-04-27 22:28:57 +0800

    Update Title.js

--- themes/example/components/Title.js
+++ themes/example/components/Title.js
@@ -9,7 +9,25 @@ import { siteConfig } from '@/lib/config'
 export const Title = (props) => {
   const { post } = props
   const title = post?.title || "Welcome,This is Li Zhongping's blog."
-  const description = post ? "by Li ZhongPing." : "⛰️户外摄影\u00A0\u00A0🚴骑行\u00A0\u00A0⛺️背包徒步\u00A0\u00A0🛰️业余无线电"
+  const description = post ? "by Li ZhongPing." : (
+    <React.Fragment>
+      <Link href={`https://lizhongping.asia/tag/outdoor-photography`} passHref>
+        <a className="hover:underline">⛰️户外摄影</a>
+      </Link>
+      <span>&#160;&#160;</span>  {/* 添加空格 */}
+      <Link href={`https://lizhongping.asia/tag/cycling`} passHref>
+        <a className="hover:underline">🚴骑行</a>
+      </Link>
+      <span>&#160;&#160;</span>  {/* 添加空格 */}
+      <Link href={`https://lizhongping.asia/tag/hiking`} passHref>
+        <a className="hover:underline">⛺️背包徒步</a>
+      </Link>
+      <span>&#160;&#160;</span>  {/* 添加空格 */}
+      <Link href={`https://lizhongping.asia/tag/amateur-radio`} passHref>
+        <a className="hover:underline">🛰️业余无线电</a>
+      </Link>
+    </React.Fragment>
+  );
 
   return (
     <div className="text-center px-6 py-12 mb-6 bg-gray-100 dark:bg-hexo-black-gray dark:border-hexo-black-gray border-b">
