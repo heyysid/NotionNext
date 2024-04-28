9dca1df95feb11d9eb20b546d755d97a1b05ea02
BD7PCI <112547623+heyysid@users.noreply.github.com>
2024-04-28 10:17:24 +0800

    Update SideBar.js

--- themes/example/components/SideBar.js
+++ themes/example/components/SideBar.js
@@ -10,7 +10,7 @@ import Progress from './Progress'
 export const SideBar = (props) => {
   const { locale } = useGlobal()
   const { post, latestPosts, categoryOptions, notice, tagsptions } = props
-  const description = post ? <Progress targetRef={someTargetRef} showPercent={true} /> : (
+  const description = post ? <Progress /> : (
     <div className="w-full md:w-64 sticky top-8">
         <aside className="rounded shadow overflow-hidden mb-6">
           <h3 className="text-sm bg-gray-100 text-gray-700 dark:bg-hexo-black-gray dark:text-gray-200 py-3 px-4 dark:border-hexo-black-gray border-b">{locale.COMMON.CATEGORY}</h3>
