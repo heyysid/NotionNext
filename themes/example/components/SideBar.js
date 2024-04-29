7557e60bdd3003f7500835fba536b6f1872e249e
BD7PCI <112547623+heyysid@users.noreply.github.com>
2024-04-29 11:49:03 +0800

    Update SideBar.js

--- themes/example/components/SideBar.js
+++ themes/example/components/SideBar.js
@@ -11,7 +11,7 @@ export const SideBar = (props) => {
   const { locale } = useGlobal()
   const { latestPosts, post, categoryOptions, notice, tagsptions } = props
   return (         
-      <div className="w-full md:w-64 sticky top-8">
+      <div className="w-full md:w-4/5 sticky top-8">
         <aside className="rounded shadow overflow-hidden mb-6">
           <h3 className="text-sm bg-gray-100 text-gray-700 dark:bg-hexo-black-gray dark:text-gray-200 py-3 px-4 dark:border-hexo-black-gray border-b"> {post ? "进度条" : locale.COMMON.CATEGORY} </h3>
           <div className="p-4">
