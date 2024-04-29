41457cfe41fe95b9869925f688d21f1bec5fc60c
BD7PCI <112547623+heyysid@users.noreply.github.com>
2024-04-29 17:55:37 +0800

    Update Footer.js

--- themes/example/components/Footer.js
+++ themes/example/components/Footer.js
@@ -12,12 +12,12 @@ export const Footer = (props) => {
     
     <div className="container mx-auto max-w-4xl py-6 md:flex flex-wrap md:flex-no-wrap md:justify-between items-center text-sm">
         <div className='text-center'> &copy;{`${copyrightDate}`} @LiZhongPing版权所有.</div>
-        <div className="md:p-0 text-center md:text-right text-xs">
+        <div className="md:p-0 text-center md:text-right">
                 {/* 右侧链接 */}
                 {/* <a href="#" className="text-black no-underline hover:underline">Privacy Policy</a> */}
                 {siteConfig('BEI_AN') && (<a href="https://beian.miit.gov.cn/" className="text-black dark:text-gray-200 no-underline hover:underline ml-4">{siteConfig('BEI_AN')} </a>)}
                 <span className='dark:text-gray-200 no-underline ml-4'>
-                    由GitHub项目
+                    本站由GitHub项目
                     <a href="https://github.com/tangly1024/NotionNext" className=' hover:underline'> @NotionNext {siteConfig('VERSION')} 提供技术驱动 </a>
                 </span>
             </div>
