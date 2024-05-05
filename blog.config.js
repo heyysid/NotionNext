d5eafdde50122a1c0b44e4bd6f655e2ff6074780
BD7PCI <112547623+heyysid@users.noreply.github.com>
2024-05-06 02:46:36 +0800

    Update blog.config.js

--- blog.config.js
+++ blog.config.js
@@ -26,7 +26,7 @@ const BLOG = {
   AUTHOR: process.env.NEXT_PUBLIC_AUTHOR || 'Li ZhongPing.', // 您的昵称 例如 tangly1024
   BIO: process.env.NEXT_PUBLIC_BIO || '户外摄影、骑行、背包徒步、业余无线电，马督工粉丝', // 作者简介
   LINK: process.env.NEXT_PUBLIC_LINK || 'https://lizhongping.asia', // 网站地址
-  KEYWORDS: process.env.NEXT_PUBLIC_KEYWORD || 'Notion, 博客', // 网站关键词 英文逗号隔开
+  KEYWORDS: process.env.NEXT_PUBLIC_KEYWORD || '户外摄影、骑行、背包徒步、业余无线电', // 网站关键词 英文逗号隔开
 
   // 社交链接，不需要可留空白，例如 CONTACT_WEIBO:''
   CONTACT_EMAIL: process.env.NEXT_PUBLIC_CONTACT_EMAIL || 'heyysid@icloud.com', // 邮箱地址 例如mail@tangly1024.com
