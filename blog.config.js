50fae96748fa432187b5a7a8b0318c1f0eb9ccb1
BD7PCI <112547623+heyysid@users.noreply.github.com>
2024-05-09 14:53:51 +0800

    Update blog.config.js

--- blog.config.js
+++ blog.config.js
@@ -24,9 +24,9 @@ const BLOG = {
   CUSTOM_MENU: process.env.NEXT_PUBLIC_CUSTOM_MENU || true, // 支持Menu 类型，从3.12.0版本起，各主题将逐步支持灵活的二级菜单配置，替代了原来的Page类型，此配置是试验功能、默认关闭。
 
   AUTHOR: process.env.NEXT_PUBLIC_AUTHOR || 'Li ZhongPing.', // 您的昵称 例如 tangly1024
-  BIO: process.env.NEXT_PUBLIC_BIO || '户外摄影、骑行、背包徒步、业余无线电，马督工粉丝', // 作者简介
+  BIO: process.env.NEXT_PUBLIC_BIO || '户外摄影、骑行、背包徒步、业余无线电', // 作者简介
   LINK: process.env.NEXT_PUBLIC_LINK || 'https://lizhongping.asia', // 网站地址
-  KEYWORDS: process.env.NEXT_PUBLIC_KEYWORD || '户外摄影、骑行、背包徒步、业余无线电', // 网站关键词 英文逗号隔开
+  KEYWORDS: process.env.NEXT_PUBLIC_KEYWORD || '户外,徒步,摄影,骑行,背包徒步,业余无线电,HDR,PT', // 网站关键词 英文逗号隔开
 
   // 社交链接，不需要可留空白，例如 CONTACT_WEIBO:''
   CONTACT_EMAIL: process.env.NEXT_PUBLIC_CONTACT_EMAIL || 'heyysid@icloud.com', // 邮箱地址 例如mail@tangly1024.com
@@ -44,7 +44,7 @@ const BLOG = {
   BLOG_FAVICON: process.env.NEXT_PUBLIC_FAVICON || '/favicon.ico', // blog favicon 配置, 默认使用 /public/favicon.ico，支持在线图片，如 https://img.imesong.com/favicon.png
 
   IMAGE_COMPRESS_WIDTH: process.env.NEXT_PUBLIC_IMAGE_COMPRESS_WIDTH || 800, // 图片压缩宽度默认值，作用于博客封面和文章内容 越小加载图片越快
-  IMAGE_ZOOM_IN_WIDTH: process.env.NEXT_PUBLIC_IMAGE_ZOOM_IN_WIDTH || 1920, // 文章图片点击放大后的画质宽度，不代表在网页中的实际展示宽度
+  IMAGE_ZOOM_IN_WIDTH: process.env.NEXT_PUBLIC_IMAGE_ZOOM_IN_WIDTH || 8290, // 文章图片点击放大后的画质宽度，不代表在网页中的实际展示宽度
   RANDOM_IMAGE_URL: process.env.NEXT_PUBLIC_RANDOM_IMAGE_URL || '', // 随机图片API,如果未配置下面的关键字，主页封面，头像，文章封面图都会被替换为随机图片
   RANDOM_IMAGE_REPLACE_TEXT:
     process.env.NEXT_PUBLIC_RANDOM_IMAGE_NOT_REPLACE_TEXT ||
