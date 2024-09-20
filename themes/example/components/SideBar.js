f82355fd1e9f9a145c7a0dfe67f1deb2a4853915
BD7PCI <112547623+heyysid@users.noreply.github.com>
2024-09-20 17:42:06 +0800

    Update SideBar.js

--- themes/example/components/SideBar.js
+++ themes/example/components/SideBar.js
@@ -70,9 +70,9 @@ export const SideBar = (props) => {
                    <a className="cursor-pointer inline-block rounded hover:bg-gray-500 hover:text-white duration-200 mr-2 py-1 px-2 text-xs whitespace-nowrap dark:hover:text-white text-gray-600 hover:shadow-xl dark:border-gray-400 bg-transparent dark:bg-gray-800 text-gray-darkest hover:underline ">
                     <div className='font-light dark:text-gray-400'><i className='mr-1 fas fa-tag' />摄影</div></a>
                  </Link>
-                 <Link href={`https://lizhongping.asia/category/项目`} passHref>
+                 <Link href={`https://lizhongping.asia/category/计划`} passHref>
                    <a className="cursor-pointer inline-block rounded hover:bg-gray-500 hover:text-white duration-200 mr-2 py-1 px-2 text-xs whitespace-nowrap dark:hover:text-white text-gray-600 hover:shadow-xl dark:border-gray-400 bg-transparent dark:bg-gray-800 text-gray-darkest hover:underline ">
-                    <div className='font-light dark:text-gray-400'><i className='mr-1 fas fa-folder' />项目</div></a>
+                    <div className='font-light dark:text-gray-400'><i className='mr-1 fas fa-folder' />计划</div></a>
                  </Link>
                  <Link href={`https://lizhongping.asia/category/资源`} passHref>
                    <a className="cursor-pointer inline-block rounded hover:bg-gray-500 hover:text-white duration-200 mr-2 py-1 px-2 text-xs whitespace-nowrap dark:hover:text-white text-gray-600 hover:shadow-xl dark:border-gray-400 bg-transparent dark:bg-gray-800 text-gray-darkest hover:underline ">
