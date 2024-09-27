46283092862fdad8a458ab3250146532a2601980
BD7PCI <112547623+heyysid@users.noreply.github.com>
2024-09-28 00:29:44 +0800

    Update SideBar.js

--- themes/example/components/SideBar.js
+++ themes/example/components/SideBar.js
@@ -72,15 +72,15 @@ export const SideBar = (props) => {
                  </Link>
                  <Link href={`https://lizhongping.asia/category/计划`} passHref>
                    <a className="cursor-pointer inline-block rounded hover:bg-gray-500 hover:text-white duration-200 mr-2 py-1 px-2 text-xs whitespace-nowrap dark:hover:text-black dark:hover:bg-white dark:text-white hover:shadow-xl duration-200 bg-transparent hover:underline ">
-                    <div className='font-light'><img src="/folder.gif" className='inline-block mr-1' alt="folder icon" />计划</div></a>
+                    <div className='font-light'><img src="/image3.gif" className='inline-block mr-1' alt="image3.gif icon" />计划</div></a>
                  </Link>
                  <Link href={`https://lizhongping.asia/category/资源`} passHref>
                    <a className="cursor-pointer inline-block rounded hover:bg-gray-500 hover:text-white duration-200 mr-2 py-1 px-2 text-xs whitespace-nowrap dark:hover:text-black dark:hover:bg-white dark:text-white hover:shadow-xl duration-200 bg-transparent hover:underline ">
-                    <div className='font-light'><img src="/folder.gif" className='inline-block mr-1' alt="folder icon" />资源</div></a>
+                    <div className='font-light'><img src="/comp.blue.gif" className='inline-block mr-1' alt="comp.blue icon" />资源</div></a>
                  </Link>
                  <Link href={`https://lizhongping.asia/category/图片`} passHref>
                    <a className="cursor-pointer inline-block rounded hover:bg-gray-500 hover:text-white duration-200 mr-2 py-1 px-2 text-xs whitespace-nowrap dark:hover:text-black dark:hover:bg-white dark:text-white hover:shadow-xl duration-200 bg-transparent hover:underline ">
-                    <div className='font-light'><img src="/folder.gif" className='inline-block mr-1' alt="folder icon" />图片</div></a>
+                    <div className='font-light'><img src="/image2.gif" className='inline-block mr-1' alt="image icon" />图片</div></a>
                  </Link>
                  <Link href={`https://lizhongping.asia/category/文章`} passHref>
                    <a className="cursor-pointer inline-block rounded hover:bg-gray-500 hover:text-white duration-200 mr-2 py-1 px-2 text-xs whitespace-nowrap dark:hover:text-black dark:hover:bg-white dark:text-white hover:shadow-xl duration-200 bg-transparent hover:underline ">
