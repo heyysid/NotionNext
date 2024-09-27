a96687e859f4fd8a07478edc962c1a0b5a9b4565
BD7PCI <112547623+heyysid@users.noreply.github.com>
2024-09-28 02:35:11 +0800

    Update hualang.js

--- pages/hualang.js
+++ pages/hualang.js
@@ -3,14 +3,14 @@ import React from 'react'
 const Hualang = () => {
   const images = [
     {
+      src: '/20140606_102418_IMGP0297_hdr_rec2020_pq_yuv444_full_cq10.avif',
       title: '这是一个完美的HDR图片示例',
       description: 'form https://people.csail.mit.edu/ericchan/hdr/avif_images/20140606_102418_IMGP0297.jpg'
-      src: '/20140606_102418_IMGP0297_hdr_rec2020_pq_yuv444_full_cq10.avif',
     },
     {
+      src: '/PQ_10_to_1000nits.avif',
       title: 'PQ 10 to 1000nits',
       description: '这是一张展示HDR效果的图片，它的动态范围从10到1000nits'
-      src: '/PQ_10_to_1000nits.avif',
     },
     
   ];
