7736f9b09a73bb2419aff9013ca04325040dcf34
BD7PCI <112547623+heyysid@users.noreply.github.com>
2024-10-12 02:43:38 +0800

    Update hualang.js

--- pages/hualang.js
+++ pages/hualang.js
@@ -32,15 +32,30 @@ const Hualang = () => {
       title:'姊妹湖',
       description:'2024年6月9日，在四川省甘孜藏族自治州巴塘县'
     },
+    {
+      src:'/乌鲁木齐秋jpgSDR3.jpg',
+      title:'乌鲁木齐秋jpgSDR3',
+      description:'2024年10月11日，在乌鲁木齐'
+    },
+    {
+      src:'/乌鲁木齐秋jpgSDR5.jpg',
+      title:'乌鲁木齐秋jpgSDR5',
+      description:'2024年10月11日，在乌鲁木齐'
+    },
+    {
+      src:'/乌鲁木齐秋jpgSDR.jpg',
+      title:'乌鲁木齐秋jpgSDR',
+      description:'2024年10月11日，在乌鲁木齐'
+    },
   ];
 
 return (
     <div>
       <h1>《画廊》</h1>
       <p>这是我的画廊页面，展示一些作品和图片。</p>
-      
+      <p>// 浏览指引</p>
       <p style={{ margin: '20px 0' }}>
-        本页的图片大部分是HDR，请先确认您当前观看环境是否能正确预览HDR效果，您可以使用下方的web工具检查当前观看环境是否已支持HDR。
+        本页的图像大部分是HDR，请先确认您当前观看环境是否能正确预览HDR效果，您可以使用下方的web工具检查当前观看环境是否支持预览HDR。
         <a target="_blank" rel="noopener noreferrer" href="https://www.wide-gamut.com/" className="hover:underline text-blue-500">widegamut「HDR」测试工具</a>
         ; 
         <a target="_blank" rel="noopener noreferrer" href="https://alexfry.github.io/ACES_ODT_Candidates_Examples/diagnostic.html" className="hover:underline text-blue-500">SDR和HDR对比</a>
@@ -48,7 +63,7 @@ return (
       </p>
       
       <p style={{ margin: '20px 0' }}>
-        如果您确认您的硬件环境支持HDR，但您仍然无法正确预览本页面的HDR，请确认您是否使用的是Chrome浏览器的最新版本，然后请确保Chrome的开发者实验室栏目里对应HDR的功能没有被禁用：
+        如果您确认您的硬件环境支持HDR，但您仍然无法正确预览本页面的HDR图像，请确认使用的Chrome浏览器版本是否为最新版，然后在Chrome的开发者实验室栏目里找到对应HDR的功能，确保HDR没有被禁用：
         <a target="_blank" rel="noopener noreferrer" href="https://lizhongping.asia/article/HDR#1d7870ed2ca542c6847abb06b3675b94" className="hover:underline text-blue-500">Chrome预览HDR重要注意事项</a>
       </p>
       
