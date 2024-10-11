313092ec6f2904b7523fe32693c6ac760f51b8b3
BD7PCI <112547623+heyysid@users.noreply.github.com>
2024-10-12 02:48:37 +0800

    Update hualang.js

--- pages/hualang.js
+++ pages/hualang.js
@@ -53,7 +53,7 @@ return (
     <div>
       <h1>《画廊》</h1>
       <p>这是我的画廊页面，展示一些作品和图片。</p>
-      <p>// 浏览指引</p>
+      <p>浏览指引>>></p>
       <p style={{ margin: '20px 0' }}>
         本页的图像大部分是HDR，请先确认您当前观看环境是否能正确预览HDR效果，您可以使用下方的web工具检查当前观看环境是否支持预览HDR。
         <a target="_blank" rel="noopener noreferrer" href="https://www.wide-gamut.com/" className="hover:underline text-blue-500">widegamut「HDR」测试工具</a>
